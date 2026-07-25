e4c0090e84ef636752cb79490e039fbfb2fd8221
manoj-mutireddygari <mutireddymanoj2305@gmail.com>
2026-07-26 01:44:05 +0530

    changes the url in the extensions

--- extension/src/constants/index.ts
+++ extension/src/constants/index.ts
@@ -8,7 +8,7 @@ export const STORAGE_KEYS = {
   repository: "supabase_repository"
 } as const;
 export const GITHUB_API = "https://api.github.com";
-export const DASHBOARD_URL = "http://localhost:3000";
+export const DASHBOARD_URL = import.meta.env.VITE_DASHBOARD_URL || "https://code-vault-pi-gray.vercel.app";
 export const SUPABASE_URL = "https://voreuakedpqlwyboitln.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvcmV1YWtlZHBxbHd5Ym9pdGxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5MTIzODYsImV4cCI6MjEwMDQ4ODM4Nn0.7Jztk9I1E0XkGZa7bmAVC4A14gOMPXm2Ppnu8-PmSfg";
 
